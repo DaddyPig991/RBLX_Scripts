516b973ea8b09860ee081e992f38946995e19a29
DaddyPig991 <111451749+DaddyPig991@users.noreply.github.com>
2025-08-02 17:22:05 +0300

    Update Hitbox.lua

--- 1BR/Hitbox.lua
+++ 1BR/Hitbox.lua
@@ -2,22 +2,33 @@ local Players = game:GetService("Players")
 local CollectionService = game:GetService("CollectionService")
 
 local Hitbox = {
-  Enabled = false,
-  TargetNPC = true,
-  TargetPlayers = false,
-  Size = 1,
-  Targets = {
-    "Head",
-    "Torso",
-    "Right Arm",
-    "Left Arm",
-    "Right Leg",
-    "Left Leg",
-  }
+	  Enabled = false,
+	  TargetNPC = true,
+	  TargetPlayers = false,
+	  Size = 1,
+	  Targets = {
+	    "Head",
+	    "Torso",
+	    "Right Arm",
+	    "Left Arm",
+	    "Right Leg",
+	    "Left Leg",
+	  },
+	CurrentTarget = "Head"
 }
 
 function Hitbox:UpdateHitbox(entity)
-  print("UPDATING HITBOX")
+	local targetPart = entity:FindFirstChild(Hitbox.CurrentTarget)
+
+	if targetPart then
+		targetPart.Size = Vector3.new(Hitbox.Size, Hitbox.Size, Hitbox.Size)
+
+		targetPart.Material = "Plastic"
+
+		targetPart.Transparency = 0.5
+
+		targetPart.CanCollide = false
+	end
 end
 
 local function IsValidNPC(model)
@@ -29,18 +40,22 @@ local function IsValidNPC(model)
 end
 
 local function AddNPC(npc)
-	if IsValidNPC(npc) then
+	if IsValidNPC(npc) and Hitbox.TargetNPC then
 		Hitbox:UpdateHitbox(npc)
 	end
 end
 
 Players.PlayerAdded:Connect(function(player)
-	Hitbox:UpdateHitbox(player)
+	if Hitbox.TargetPlayers then
+		Hitbox:UpdateHitbox(player)
+	end
 end)
 
 for _, player in ipairs(Players:GetPlayers()) do
 	if player ~= LocalPlayer then
-		Hitbox:UpdateHitbox(player)
+		if Hitbox.TargetPlayers then
+			Hitbox:UpdateHitbox(player)
+		end
 	end
 end
 
